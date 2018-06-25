0bed8edfddd8bcdd121c80fdecb50b62fc987843
Amit Kumar <verma.amit0407@gmail.com>
2018-06-25 23:20:27 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
 var cacheName = 'weatherPWA-v1';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/scripts/app.js',
-  '/scripts/localforage-1.4.0.js',
-  '/styles/ud811.css',
-  '/images/clear.png',
-  '/images/cloudy-scattered-showers.png',
-  '/images/cloudy.png',
-  '/images/fog.png',
-  '/images/ic_add_white_24px.svg',
-  '/images/ic_refresh_white_24px.svg',
-  '/images/partly-cloudy.png',
-  '/images/rain.png',
-  '/images/scattered-showers.png',
-  '/images/sleet.png',
-  '/images/snow.png',
-  '/images/thunderstorm.png',
-  '/images/wind.png'
+  '/ud811/',
+  '/ud811/index.html',
+  '/ud811/scripts/app.js',
+  '/ud811/scripts/localforage-1.4.0.js',
+  '/ud811/styles/ud811.css',
+  '/ud811/images/clear.png',
+  '/ud811/images/cloudy-scattered-showers.png',
+  '/ud811/images/cloudy.png',
+  '/ud811/images/fog.png',
+  '/ud811/images/ic_add_white_24px.svg',
+  '/ud811/images/ic_refresh_white_24px.svg',
+  '/ud811/images/partly-cloudy.png',
+  '/ud811/images/rain.png',
+  '/ud811/images/scattered-showers.png',
+  '/ud811/images/sleet.png',
+  '/ud811/images/snow.png',
+  '/ud811/images/thunderstorm.png',
+  '/ud811/images/wind.png'
 ];
 
 self.addEventListener('install', function(e) {
